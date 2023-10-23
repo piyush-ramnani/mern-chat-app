d679801c91d52ebfbba75f62ad928c64ce02fb87
testGit <test@Git.com>
2023-10-23 22:46:10 +0530

    Create-group-chat functionality added

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -51,10 +51,56 @@ const accessChat = expressAsyncHandler(async (req, res) => {
 
 const getChats = expressAsyncHandler(async (req, res) => {
   try {
-    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } }).then((result) =>
-      res.send(result)
-    );
-  } catch (error) {}
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
 });
 
-module.exports = { accessChat, getChats };
+const createGroupChat = expressAsyncHandler(async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "please fill all the fields" });
+  }
+
+  var users = JSON.parse(req.body.users);
+
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .send({ message: "More than 2 users required for a group chat!" });
+  }
+
+  users.push(req.user); //current user who is creating the group chat
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(200).json(fullGroupChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = { accessChat, getChats, createGroupChat };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat, getChats } = require("../controllers/chatControllers");
+const {
+  accessChat,
+  getChats,
+  createGroupChat,
+} = require("../controllers/chatControllers");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, getChats);
-// router.route("/group").post(protect, createGroup);
+router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
 // router.route("/groupadd").put(protect, addToGroup);
