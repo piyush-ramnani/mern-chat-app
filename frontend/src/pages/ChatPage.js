5eab1b33f3fd9ba892a298f79f825ab5cdcfc7c4
piyush-ramnani <test@Git.com>
2023-12-24 16:32:39 +0530

    updated pages

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,30 +1,22 @@
-import { useEffect } from "react";
-import axios from "axios";
+import { Box } from "@chakra-ui/layout";
+import Chatbox from "../components/ChatBox";
+import MyChats from "../components/MyChats";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { ChatState } from "../Context/ChatProvider";
 
 const ChatPage = () => {
-  //Async simply allows us to write promises-based code as if it was synchronous It makes sure that a promise is returned and if it is not returned then JavaScript automatically wraps it in a promise which is resolved with its value.
+  const { user } = ChatState();
+  console.log(user);
 
-  //Await function is used to wait for the promise. It could be used within the async block only. It makes the code wait until the promise returns a result. It only makes the async block wait.
-  const fetchChats = async () => {
-    const { data } = await axios.get("/api/chat");
-
-    console.log(data);
-  };
-
-  //The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are: fetching data, directly updating the DOM, and timers. useEffect accepts two arguments. The second argument is optional: useEffect(<function>, <dependency>). By using [] dependency in the below function it only renders the first time.
-
-  /* 
-  EXAMPLE:
-  useEffect(() => {
-    setCalculation(() => count * 2);
-  }, [count]); // <- Hook dependent on count variable, renders whenever count changes
-  */
-
-  useEffect(() => {
-    fetchChats();
-  }, []);
-
-  return <div>Chat Page</div>;
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box>
+        {user && <MyChats />}
+        {user && <Chatbox />}
+      </Box>
+    </div>
+  );
 };
 
 export default ChatPage;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
 import { Box, Container, Text } from "@chakra-ui/react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import Login from "../components/authentication/Login";
 import Signup from "../components/authentication/Signup";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const HomePage = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats"); //if logged in, move to chat page
+  }, [history]);
+
   return (
     <Container maxW={"l"} centerContent>
       <Box
